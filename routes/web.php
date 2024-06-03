9709affff036dbc622dd5f3e390d38018bf15256
quentinmdl <quentin.mendel.pro@gmail.com>
2024-06-03 17:53:26 +0200

    feat: clean project

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MovieController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-Route::apiResource('/movies',MovieController::class);
 
